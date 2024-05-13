675394469656a3c66ff2bc646181907479ad37c8
I Made Purnama Yasa <62276883+imadepurnamayasa@users.noreply.github.com>
2024-05-14 04:59:48 +0800

    CHANGE

--- login/header.php
+++ login/header.php
@@ -20,8 +20,8 @@ if (!isset($_SESSION['user'])) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Modernize Free</title>
-    <link rel="shortcut icon" type="image/png" href="assets/images/logos/favicon.png" />
+    <title>Login</title>
+    <!-- <link rel="shortcut icon" type="image/png" href="assets/images/logos/favicon.png" /> -->
     <link rel="stylesheet" href="assets/css/styles.min.css" />
 </head>
 
